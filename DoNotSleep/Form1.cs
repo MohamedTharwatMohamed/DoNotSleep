11e4b023bfef83599d0e98d6d50552fcd93f27af
Mohammed Tharwat <m.th001@hotmail.com>
2020-04-30 20:43:22 +0300

    Add project files.

---
+++ DoNotSleep/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoNotSleep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isStop = false;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Working!";
+
+            await Task.Run(() =>
+            {
+                moveTheMouse();
+            });
+        }
+
+        void moveTheMouse()
+        {
+            while (!isStop)
+            {
+                Cursor.Position = new Point(Cursor.Position.X - 5, Cursor.Position.Y - 5);
+                Thread.Sleep(5000);
+                Cursor.Position = new Point(Cursor.Position.X + 5, Cursor.Position.Y + 5);
+            }
+
+        }
+       
+
+    }
+}
---
+++ NoSleep/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoSleep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isStop = false;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Working!";
+
+            await Task.Run(() =>
+            {
+                moveTheMouse();
+            });
+        }
+
+        void moveTheMouse()
+        {
+            while (!isStop)
+            {
+                Cursor.Position = new Point(Cursor.Position.X - 5, Cursor.Position.Y - 5);
+                Thread.Sleep(5000);
+                Cursor.Position = new Point(Cursor.Position.X + 5, Cursor.Position.Y + 5);
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isStop = true;
+            label2.Text = "Stoped!";
+
+        }
+
+    }
+}
